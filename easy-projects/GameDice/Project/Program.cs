a58988ed9a6eb668225fbe3775c8417073879955
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-09-23 18:02:02 -0300

    solved game dice

---
+++ easy-projects/GameDice/Project/Program.cs
@@ -0,0 +1,33 @@
+﻿Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+if(roll1 == roll2){
+    Console.WriteLine("BONUS: Prémio em dobro!");
+    total = total * 2;
+}
+
+if(roll2 == roll3){
+    Console.WriteLine("BONUS: Prémio em dobro!");
+    total = total * 2;
+}
+
+if(roll1 == roll3){
+    Console.WriteLine("BONUS: Prémio em dobro!");
+    total = total * 2;
+}
+
+if(roll1 == roll2 & roll2 == roll3 & roll1 == roll3){
+    Console.WriteLine("BONUS: Todos os dados são iguais! Ganhou +6!");
+    total += 6;
+}
+
+if(total >= 15){
+    Console.WriteLine($"Você venceu o jogo! Pontuação: | {total} |");
+}else{
+    Console.WriteLine($"Você perdeu! Pontuação | {total} |");
+}
---
+++ easy-projects/GameDice/README.md
@@ -0,0 +1,8 @@
+# Game Dices
+Você usará o método Random.Next() para simular a rolagem de três dados de seis lados cada. Você avaliará os valores para calcular a pontuação. Se a pontuação for superior a 15, você exibirá uma menssagem de vitória para o usuário. Caso contrário você exibirá uma menssagem de derrota para o usuário.
+
+## Regras
+- Se quaisquer dos dados rolados resultarem no mesmo valor, você receberá dois pontos de bônus pelo resultado duplicado.
+- Se os três dados rolados resultarem no mesmo valor, você receberá 6 pontos ded bônus pelo resultado triplicado.
+- Se a soma de três dados rolados, mais quaisquer pontos de bônus, for igual ou maior que 15, você vencerá o jogo.
+Caso contrário, você perderá.
