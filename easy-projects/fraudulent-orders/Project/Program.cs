24ebf37716a84c467d088763b345e2758d02effb
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-09-30 13:47:24 -0300

    feat: solved freaudulent orders project

--- easy-projects/fraudulent-orders/Project/Program.cs
+++ easy-projects/fraudulent-orders/Project/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ids = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach(string id in ids){
+    if(id.StartsWith("B")){
+        Console.WriteLine(id);
+    }
+}
