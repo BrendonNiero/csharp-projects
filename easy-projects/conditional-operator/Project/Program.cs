54fe4d6b58f6bb150dec72677c5e5e9d7546d948
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-11-12 23:05:12 -0300

    feat: new resolution at conditional operator

--- easy-projects/conditional-operator/Project/Program.cs
+++ easy-projects/conditional-operator/Project/Program.cs
@@ -1,4 +1,5 @@
-﻿int saleAmount = 1100;
+﻿int saleAmount = 1010;
 int discount = saleAmount > 1000 ? 100 : 50;
 
-Console.WriteLine($"Discount: {discount}");
+Console.WriteLine($"You have to pay: {(saleAmount - discount)}");
+Console.WriteLine($"Sala value: {saleAmount}. Discount: {discount}");
