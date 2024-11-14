9cad694123b890991a5744cc93d8ad07528a042d
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-11-13 23:54:18 -0300

    init: decision-logic

---
+++ easy-projects/decision-logic/Project/Program.cs
@@ -0,0 +1,8 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+if(permission.Contains("Admin"))
+{
+    level > 55 && Console.WriteLine("Welcome, Super admin user.");
+    level <= 55 && Console.WriteLine("Welcome, admin user.");
+}
