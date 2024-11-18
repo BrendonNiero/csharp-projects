1026b8ec47bcafd5bc7a19e8eb75e8741bd169ca
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2024-11-18 18:10:26 -0300

    refact: refactoring Fizz Buzz Project

--- easy-projects/fizz-buzz/Project/Program.cs
+++ easy-projects/fizz-buzz/Project/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for(int count = 1; count <= 100; count++)
+{
+    if(count % 5 == 0 && count % 3 ==0)
+        Console.WriteLine($"{count} - FizzBuzz");
+    else if(count % 3 == 0)
+        Console.WriteLine($"{count} - Fizz");
+    else if(count % 5 == 0)
+        Console.WriteLine($"{count} - Buzz");
+    else
+        Console.WriteLine(count);
+}
